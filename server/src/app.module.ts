61c38dd9173ff67cd8a278b7cf18c3331fc682f9
Thales <thales.souz@outlook.com>
2024-02-29 21:28:29 -0300

    build: install sequelize and config module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { CustomerModule, ProductsModule } from '@infra';
 
 @Module({
-  imports: [CustomerModule, ProductsModule],
+  imports: [ConfigModule CustomerModule, ProductsModule],
   controllers: [],
   providers: [],
 })
