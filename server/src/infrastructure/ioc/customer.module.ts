1240665a2a3d868365b61ac59adaa320b84f9a3e
Thales <thales.souz@outlook.com>
2024-03-07 15:51:09 -0300

    refact: remove redis caching for vercel deploy

--- server/src/infrastructure/ioc/customer.module.ts
+++ server/src/infrastructure/ioc/customer.module.ts
@@ -2,41 +2,33 @@ import { Module } from '@nestjs/common';
 import { AuthController } from '@presentation/controller/auth.controller';
 import { ApplicationModule } from '@app/application.module';
 import { CustomerController } from '@presentation/controller/customer.controller';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import {
-  CacheInterceptor,
-  CacheModule,
-  CacheStore,
-} from '@nestjs/cache-manager';
-import { redisStore } from 'cache-manager-redis-store';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 import { DomainModule } from '../../domain/domain.module';
 
 @Module({
   imports: [
     ApplicationModule,
     DomainModule,
-    CacheModule.registerAsync({
-      isGlobal: true,
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        ttl: 60,
-        max: 5,
-        store: redisStore as unknown as CacheStore,
-        socket: {
-          host: configService.getOrThrow<string>('REDIS_HOST'),
-          port: configService.getOrThrow<string>('REDIS_PORT'),
-        },
-      }),
-      inject: [ConfigService],
-    }),
+    // CacheModule.registerAsync({
+    //   isGlobal: true,
+    //   imports: [ConfigModule],
+    //   useFactory: async (configService: ConfigService) => ({
+    //     ttl: 60,
+    //     max: 5,
+    //     store: redisStore as unknown as CacheStore,
+    //     socket: {
+    //       host: configService.getOrThrow<string>('REDIS_HOST'),
+    //       port: configService.getOrThrow<string>('REDIS_PORT'),
+    //     },
+    //   }),
+    //   inject: [ConfigService],
+    // }),
   ],
   controllers: [AuthController, CustomerController],
   providers: [
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CacheInterceptor,
-    },
+    // {
+    //   provide: APP_INTERCEPTOR,
+    //   useClass: CacheInterceptor,
+    // },
   ],
 })
 export class CustomerModule {}
--- server/src/infrastructure/ioc/product.module.ts
+++ server/src/infrastructure/ioc/product.module.ts
@@ -1,39 +1,31 @@
 import { Module } from '@nestjs/common';
 import { ApplicationModule } from '../../application/application.module';
 import { ProductController } from '../../presentation/controller/products.controller';
-import {
-  CacheInterceptor,
-  CacheModule,
-  CacheStore,
-} from '@nestjs/cache-manager';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { redisStore } from 'cache-manager-redis-store';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
     ApplicationModule,
-    CacheModule.registerAsync({
-      isGlobal: true,
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        ttl: 60,
-        max: 5,
-        store: redisStore as unknown as CacheStore,
-        socket: {
-          host: configService.getOrThrow<string>('REDIS_HOST'),
-          port: configService.getOrThrow<string>('REDIS_PORT'),
-        },
-      }),
-      inject: [ConfigService],
-    }),
+    // CacheModule.registerAsync({
+    //   isGlobal: true,
+    //   imports: [ConfigModule],
+    //   useFactory: async (configService: ConfigService) => ({
+    //     ttl: 60,
+    //     max: 5,
+    //     store: redisStore as unknown as CacheStore,
+    //     socket: {
+    //       host: configService.getOrThrow<string>('REDIS_HOST'),
+    //       port: configService.getOrThrow<string>('REDIS_PORT'),
+    //     },
+    //   }),
+    //   inject: [ConfigService],
+    // }),
   ],
   controllers: [ProductController],
   providers: [
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CacheInterceptor,
-    },
+    // {
+    //   provide: APP_INTERCEPTOR,
+    //   useClass: CacheInterceptor,
+    // },
   ],
 })
 export class ProductsModule {}
